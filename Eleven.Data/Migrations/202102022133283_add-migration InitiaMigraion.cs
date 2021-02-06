2c1cbf4e0d91dd215d0263b2fa826f032a14f6ee
Meaza <meazastesfaye@gmail.com>
2021-02-06 09:05:40 -0500

    commit

---
+++ Eleven.Data/Migrations/202102022133283_add-migration InitiaMigraion.cs
@@ -0,0 +1,16 @@
+namespace Eleven.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class addmigrationInitiaMigraion : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
